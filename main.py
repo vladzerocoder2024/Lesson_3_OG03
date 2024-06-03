0fd434b25c0ca3243dfa315a8c1d83e050fe6eb2
vladzerocoder2024 <vshaduk@yandex.ru>
2024-06-03 23:34:56 +0500

    Добавили все константы

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pygame
-
+import random
 pygame.init()
 
 SCREEN_WIDHT = 800         # ширина экрана
@@ -7,7 +7,18 @@ SCREEN_HIGHT = 600         # высота экрана
 screen = pygame.display.set_mode((SCREEN_WIDHT, SCREEN_HIGHT))  # создаём окно экрана в игре
 
 pygame.display.set_caption("Игра ТИР")      # создаём заголовок окна экрана в игре
-icon = pygame.image.load("")                # загружаем в переменную icon изображение иконки игры
+icon = pygame.image.load("img/Иконка игры ТИР.jpg")   # загружаем в переменную icon изображение иконки игры
+pygame.display.set_icon(icon)                         # устанавливаем иконку
+
+target_img = pygame.image.load("img/мишень.png")      # загружаем в переменную target_img изображение цели в игре
+target_wight = 50                           # ширина цели в пикселах
+target_height = 50                          # высота цели в пикселах
+
+targer_x = random.randint(0, SCREEN_WIDHT - target_wight)   # задаём случайные координаты цели в окне
+targer_y = random.randint(0, SCREEN_HIGHT - target_height)
+
+Color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))    # задаём случайный цвет экрана игры
+
 
 
 running = True
