c6bad359d3953b9afc14d60545830daefe498718
vladzerocoder2024 <vshaduk@yandex.ru>
2024-06-04 00:12:25 +0500

    Change correct file name

--- main.py
+++ main.py
@@ -7,10 +7,10 @@ SCREEN_HIGHT = 600         # высота экрана
 screen = pygame.display.set_mode((SCREEN_WIDHT, SCREEN_HIGHT))  # создаём окно экрана в игре
 
 pygame.display.set_caption("Игра ТИР")      # создаём заголовок окна экрана в игре
-icon = pygame.image.load("img/Иконка игры ТИР.jpg")   # загружаем в переменную icon изображение иконки игры
+icon = pygame.image.load("img/icon_TIR.jpg")   # загружаем в переменную icon изображение иконки игры
 pygame.display.set_icon(icon)                         # устанавливаем иконку
 
-target_img = pygame.image.load("img/мишень.png")      # загружаем в переменную target_img изображение цели в игре
+target_img = pygame.image.load("img/target.png")      # загружаем в переменную target_img изображение цели в игре
 target_wight = 80                           # ширина цели в пикселах
 target_height = 80                          # высота цели в пикселах
 
@@ -27,4 +27,4 @@ while running:
 
 pygame.quit()
 
-# Исправили
+# Исправил название файлов на англ. без пробелов
